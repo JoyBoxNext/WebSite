ad480ee2c1aba6706b041606ab299083282fb2e3
Giyos <ilkhomovgiyos@gmail.com>
2021-09-28 20:48:24 +0500

    fixing

--- pages/index.js
+++ pages/index.js
@@ -1123,11 +1123,11 @@ export default function Home({ baraxolkaData }) {
   );
 }
 
-export const getServerSideProps = async () => {
-  const res = await getJoybox();
-  return {
-    props: {
-      baraxolkaData: res.data || [],
-    },
-  };
-};
+// export const getServerSideProps = async () => {
+//   const res = await getJoybox();
+//   return {
+//     props: {
+//       baraxolkaData: res.data || [],
+//     },
+//   };
+// };
